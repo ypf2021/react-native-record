2b7f3a2340566d450b0099d8c12e4d55aa1428d8
ypf2021 <1472809818@qq.com>
2023-12-28 17:22:15 +0800

    react-native stack路由

---
+++ App.tsx
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+// @ts-ignore
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import StackRouterComponent from './src/stackRouter';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <StackRouterComponent></StackRouterComponent>
+    </NavigationContainer>
+  );
+}
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/stackRouter.tsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import { Text, View, Button, StyleSheet } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+function HomeView(props: any): React.JSX.Element {
+    return <>
+        <View style={[style.container]}>
+            <Text> this is HomeView </Text>
+            <Button 
+             title='goto newsPage'
+             onPress={() => {props.navigation.navigate('News')}}
+            />
+        </View>
+    </>
+}
+
+function NewsView(props: any): React.JSX.Element {
+    return <>
+        <View style={[style.container]}>
+            <Text> this is newsview </Text>
+            <Button 
+             title='goto homePage'
+             onPress={() => {props.navigation.navigate('Home')}}
+            />
+        </View>
+    </>
+}
+
+function Index(): React.JSX.Element {
+    return <>
+        <Stack.Navigator initialRouteName='News'>
+            <Stack.Screen 
+             name='Home' 
+             component={HomeView}
+             options={{
+                title: 'home页面'
+             }}
+            />
+            <Stack.Screen 
+             name='News' 
+             component={NewsView}
+             options={{
+                title: 'news页面'
+             }}
+            />
+        </Stack.Navigator>
+    </>
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default Index
